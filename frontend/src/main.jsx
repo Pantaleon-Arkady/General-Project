ddada589f9af82c05f58eaa4a37943585a094ebe
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-09 10:37:20 +0800

    bootstrap style and react-bootstrap works, also installed react-bootstrap bootstrap and not react-bootstrap only

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TrialComponent from "./trials/TrialComponent.jsx";
