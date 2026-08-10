cbdff855b19809a391218c55204027a83d4b968c
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-10 14:56:57 +0800

    route init and also handshake

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TrialsController;
+
+Route::get('/handshake', [TrialsController::class, 'handShake']);
 
 Route::get('/', function () {
     return view('welcome');
