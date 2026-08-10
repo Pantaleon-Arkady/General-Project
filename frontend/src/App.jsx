c010db1bd6c46a7549feb4984a6404ca62ae9f89
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-10 10:39:06 +0800

    App Component style   - defaults - mainly box sizing border box     - border box applies only to borders and paddings   - app component - wrapper div with padding 100 height then content div with 100 height

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,12 @@ function App() {
 
   return (
     <>
-        <div>
-            <Link to="/trials">Trials</Link>
-            <Button variant="primary">Primary Button by react - bootstrap</Button>
-            <button className="btn btn-primary">Primary Button by bootstrap</button>
+        <div className="h-100 w-100 p-3 bg-white">
+            <div className="h-100 border border-2 rounded bg-light p-3">
+                <Link to="/trials" className="btn btn-primary">Trials</Link>
+                <Button variant="primary">Primary Button by react - bootstrap</Button>
+                <button className="btn btn-primary">Primary Button by bootstrap</button>
+            </div>
         </div>
     </>
   )
