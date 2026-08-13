a54562ad8134f668e476e609f4f5cc246380eaec
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-13 13:23:34 +0800

    App Initial Routing   - default route renders login component   - register route renders register component and login route renders login component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,12 +7,18 @@ import "./App.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TrialComponent from "./trials/TrialComponent.jsx";
+import Register from "./forms/Register.jsx";
+import Login from "./forms/Login.jsx";
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <StrictMode>
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<App />}>
+                    <Route index element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                </Route>
                 <Route path="/trials" element={<TrialComponent />} />
             </Routes>
         </StrictMode>
