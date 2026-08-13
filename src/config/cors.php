735ad1a4e68157ef7352b7ddb7b70eced7cd239a
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-13 16:11:46 +0800

    user register route

--- src/config/cors.php
+++ src/config/cors.php
@@ -19,6 +19,7 @@ return [
         'api/*',
         'sanctum/csrf-cookie',
         '/handshake',
+        '/register'
     ],
 
     'allowed_methods' => ['*'],
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TrialsController;
+use App\Http\Controllers\UserController;
 
+Route::post('/register', [UserController::class, 'register']);
 Route::get('/handshake', [TrialsController::class, 'handShake']);
 
 Route::get('/', function () {
