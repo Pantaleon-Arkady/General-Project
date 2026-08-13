fcf5f681a7c16efb9725176472c492e3bb43d8e6
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-13 19:43:54 +0800

    Login function init, validate and username/email check

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -4,10 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class UserController extends Controller
 {
+    public function Login(Request $request)
+    {
+        $validated = $request->validate([
+            'namemail' => [
+                'required',
+                'string'
+            ],
+            'password' => [
+                'required',
+                'string'
+            ]
+        ]);
+
+        $loginInput = $validated['namemail'];
+
+        $user = filter_var($loginInput, FILTER_VALIDATE_EMAIL)
+            ? User::where('email', $loginInput)->first()
+            : User::where('name', $loginInput)->first();
+
+        if (!$user) {
+            return response()->json([
+                'stat' => false
+            ], 401);
+        }
+
+        return response()->json([
+            'stat' => true,
+            'message' => "Login Success",
+            'user' => $loginInput
+        ]);
+    }
+
     public function Register(Request $request)
     {
         $validated = $request->validate([
