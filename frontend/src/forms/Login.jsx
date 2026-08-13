25000295547d81eb3925e21e85229fb2cf8d135e
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-13 16:48:35 +0800

    Login form - setting data for login request

--- frontend/src/forms/Login.jsx
+++ frontend/src/forms/Login.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
+
 function Login() {
+    const [namemail, setNamemail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const loginData = {
+            namemail: namemail,
+            password: password,
+        };
+
+        console.log("LOG IN");
+        console.log("Email or username: " + namemail);
+        console.log("Password: " + password);
+        console.log("loginData: ");
+        console.log(loginData);
+
+        try {
+        } catch (err) {
+            console.log(err.response);
+        }
+    }
 
     return (
         <>
             <div>
-                Login Form
+                <form onSubmit={handleSubmit}>
+                    <input
+                        name="namemail"
+                        type="text"
+                        placeholder="Email or username..."
+                        value={namemail}
+                        onChange={(e) => setNamemail(e.target.value)}
+                    />
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Enter your password..."
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button className="btn btn-success">
+                        Log in
+                    </button>
+                </form>
             </div>
         </>
     )
