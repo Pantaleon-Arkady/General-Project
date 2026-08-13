5899ae712a4a12120e5da6bb9f7d4aa58865a879
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-13 13:24:32 +0800

    App component   - Outlet for Register and Login component   - useLocation to identify url pathname and set certain values for proper display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
+import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
+import Register from './forms/Register';
+import { useEffect, useState } from 'react';
 
 function App() {
-
+    const location = useLocation();
+    const [isRegister, setIsRegister] = useState(false);
+    
+    useEffect(() => {
+        if (location.pathname === "/register") {
+            setIsRegister(true)
+        }
+    }, [location.pathname])
 
     return (
         <>
-            <div className="h-100 w-100 p-3 bg-white">
+            <div className="app_main_div h-100 w-100 bg-white">
                 <div className="h-100 border border-2 rounded bg-light p-3">
-                    
+                    <div>
+                        Current Location: {location.pathname}
+                    </div>
+                    <Outlet />
+                    <div className='border rounded'>
+                        {location.pathname === "/login" || "/" ? "Register ?" : "Login ?" }<br />
+                        {isRegister ? "Login ?" : "Register ?"}
+                    </div>
                 </div>
             </div>
         </>
