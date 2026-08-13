59cfb08542132229561feeeb44bdcd34120cc62a
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-13 20:56:43 +0800

    Home Page init

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
-import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
 import TrialComponent from "./trials/TrialComponent.jsx";
 import Register from "./forms/Register.jsx";
 import Login from "./forms/Login.jsx";
+import Home from "./pages/Home.jsx";
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <StrictMode>
             <Routes>
-                <Route path="/" element={<App />}>
+                <Route path='/' element={<App />}>
                     <Route index element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/login' element={<Login />} />
                 </Route>
-                <Route path="/trials" element={<TrialComponent />} />
+                <Route path='/trials' element={<TrialComponent />} />
+                <Route path='/home' element={<Home />} />
             </Routes>
         </StrictMode>
     </BrowserRouter>
