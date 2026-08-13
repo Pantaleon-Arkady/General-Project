39c7ae833ce6d7aeadf269963cc63d5feda73be0
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-13 14:06:53 +0800

    Register Form   - basic form elements   - register data values handled by useState   - important note, dont forget preventDefault on forms in submission logic   - register data is fine

--- frontend/src/forms/Register.jsx
+++ frontend/src/forms/Register.jsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
+
 function Register() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+            console.log("Register");
+            console.log("Username: " + username);
+            console.log("Email: " + email);
+            console.log("Password: " + password);
+        } catch (err) {
+            console.log(err.response);
+        }
+    }
 
     return (
         <>
             <div>
-                Register Form
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        name="username"
+                        type="text"
+                        placeholder="Create your username..."
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+
+                    <input
+                        name="email"
+                        type="email"
+                        placeholder="Enter your email..."
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Create a password..."
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <br />
+                    <button className="btn btn-success">
+                        Register
+                    </button>
+                </form>
             </div>
         </>
     )
