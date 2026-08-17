f5881c5a6604e923c3e877fd05151eb4b47e2838
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-17 14:27:02 +0800

    Create Notes form component init

--- frontend/src/apps/Notes.jsx
+++ frontend/src/apps/Notes.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import CreateNotes from "../forms/CreateNotes";
+
 function Notes() {
+    const [create, setCreate] = useState(false);
 
     return (
         <>
-            <div>
-                Notes component
+            <div className="border border-2 border-danger">
+                <div className="border border-2 border-primary d-flex justify-content-center p=1">
+                    <button 
+                        className="btn btn-primary m-1"
+                        onClick={() => setCreate(true)}
+                    >
+                        Add Notes
+                    </button>
+                </div>
+                <div className="p-2">
+                    Notes List Div
+                </div>
             </div>
+
+            {create && <CreateNotes />}
         </>
     )
 }
---
+++ frontend/src/forms/CreateNotes.jsx
@@ -0,0 +1,14 @@
+function CreateNotes() {
+
+    return (
+        <>
+            <div>
+                <form>
+                    Add notes
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default CreateNotes;
