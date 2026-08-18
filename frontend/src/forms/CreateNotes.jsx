03186538123d3f483c069dd6550f28f8b7f7e8eb
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-18 11:31:08 +0800

    Create note request from form component

--- frontend/src/forms/CreateNotes.jsx
+++ frontend/src/forms/CreateNotes.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react";
 import { Modal, Form, Button } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
+import axios from "../api/axios";
 
 function CreateNotes({ show, onClose }) {
+    const [note, setNote] = useState("");
+
+    const { user } = useAuth();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const noteData = {
+            note: note,
+            userId: user?.id
+        }
+
+        console.log("Note Data:");
+        console.log(noteData);
+
+        try {
+            const res = await axios.post('/create-note', noteData);
+
+            if (res.data.stat) {
+                console.log(res.data.note);
+            }
+        } catch (err) {
+            console.log(err.response)
+        }
+    }
 
     return (
         <Modal show={show} onHide={onClose} centered>
@@ -10,19 +37,23 @@ function CreateNotes({ show, onClose }) {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group>
                         <Form.Control
                             type="text"
                             placeholder="Type your note..."
                             autoFocus
+                            name="note"
+                            value={note}
+                            onChange={(e) => setNote(e.target.value)}
+                            required
                         />
                     </Form.Group>
                     <div className="border-top border-2 border-secondary my-2 p-2 d-flex flex-row justify-content-end">
                         <Button variant="secondary" onClick={onClose}>
                             Cance
                         </Button>
-                        <Button className="mx-2" variant="success">
+                        <Button className="mx-2" variant="success" type="submit">
                             Submit
                         </Button>
                     </div>
