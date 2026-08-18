8b6cb5fba9b3ff4dba16706b63d2acc0c2e8df8d
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-18 13:58:32 +0800

    Delete notes data pre setting

--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
+
 function NotesList({ notes }) {
+    const [deleteWarning, setDeleteWarning] = useState(false);
+    const [noteId, setNoteId] = useState("");
+    const [note, setNote] = useState("");
+
+    const handleDeleteWarning = (noteId, note) => {
+        setNoteId(noteId);
+        setNote(note);
+        setDeleteWarning(true);
+
+        console.log("Deleting:");
+        console.log("id: " + noteId);
+        console.log("note: " + note);
+    }
+
+    const handleDelete = () => {
+
+    }
 
     return (
         <>
             {notes.map((note, i) => (
                 <div
                     key={i}
-                    className="bg-white rounded mb-2 py-1 px-2"
+                    className="bg-white rounded mb-2 py-1 px-2 d-flex flex-row justify-content-between"
                 >
-                    {note.note}
+                    <div>
+                        {note.note}
+                    </div>
+
+                    <div>
+                        <button
+                            onClick={() => handleDeleteWarning(note.id, note.note)}
+                            className="btn btn-danger"
+                        >
+                            Delete
+                        </button>
+                    </div>
                 </div>
             ))}
         </>
