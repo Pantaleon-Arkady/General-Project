c6d695ee8f727e5395be5af676993058192c565c
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-18 13:34:46 +0800

    Notes retrieval function and user check before data query and response

--- src/app/Http/Controllers/NotesController.php
+++ src/app/Http/Controllers/NotesController.php
@@ -4,9 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Notes;
+use Illuminate\Support\Facades\Auth;
 
 class NotesController extends Controller
 {
+    public function retrieveNotes(Request $request)
+    {
+        $validated = $request->validate([
+            'userId' => ['required', 'integer']
+        ]);
+
+        $frontUser = intval($validated['userId']);
+
+        $userId = Auth::id();
+
+        if ($frontUser !== $userId) {
+            return response()->json([
+                'stat' => false,
+                'message' => "User request is not properly authenticated"
+            ], 403);
+        }
+
+        $notes = Notes::where('user_id', $frontUser)->get();
+
+        return response()->json([
+            'stat' => true,
+            // 'front' => $frontUser,
+            // 'back' => $userId
+            'notes' => $notes,
+            'message' => "Notes retrieval: OK!"
+        ], 200);
+    }
+
     public function createNote(Request $request)
     {
         $validated = $request->validate([
