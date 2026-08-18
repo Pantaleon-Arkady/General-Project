49e6d7062e83761499b0ea265d2e755504f14049
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-18 21:04:20 +0800

    Delete warning modal component

--- frontend/src/components/DeleteWarning.jsx
+++ frontend/src/components/DeleteWarning.jsx
@@ -1,19 +1,35 @@
-function DeleteWarning({ onDelete, deleteId, deleteName }) {
+import { Modal, Button } from "react-bootstrap";
 
-
+function DeleteWarning({ onDelete, deleteName, show, onClose }) {
 
     return (
         <>
-            <div>
-                Delete Warning
-
-                <div>
-                    delete id: {deleteId}
-                </div>
-                <div>
-                    deleting: {deleteName}
-                </div>
-            </div>
+            <Modal show={show} onHide={onClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Deleting Note
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>
+                        Note: <span className="fw-bold">{deleteName}</span>
+                    </div>
+                    <div className="border-top border-2 border-secondary my-2 p-2 d-flex flex-row justify-content-around">
+                        <Button
+                            variant="secondary"
+                            onClick={onClose}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            variant="danger"
+                            onClick={onDelete}
+                        >
+                            Delete
+                        </Button>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
