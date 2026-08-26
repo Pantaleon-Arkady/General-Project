3266fc79707abbab4c2ae4e38fa468873b408ddc
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-26 13:12:08 +0800

    Create task component request

--- frontend/src/forms/CreateTask.jsx
+++ frontend/src/forms/CreateTask.jsx
@@ -1,10 +1,31 @@
+import axios from "../api/axios";
+import { useState } from "react";
 import { Modal, Form, Button } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
 
 function CreateTask({show, onClose}) {
+    const [task, setTask] = useState("");
+    const { user } = useAuth();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log("Task submit");
+
+        const taskCreationData = {
+            task: task,
+            user_id: user?.id
+        }
+
+        try {
+            const res = await axios.post('/create-task', taskCreationData);
+
+            if (res.data.stat) {
+                console.log("Task creation success");
+                console.log(res.data.task);
+            }
+        } catch (err) {
+            console.log(err.response);
+        }
     }
 
     return (
@@ -17,8 +38,15 @@ function CreateTask({show, onClose}) {
             <Modal.Body>
                 <Form onSubmit={handleSubmit}>
                     <Form.Group>
-                        <Form.Control type="text" name="task" placeholder="Enter a task..." />
+                        <Form.Control
+                            type="text"
+                            name="task"
+                            placeholder="Enter a task..."
+                            value={task}
+                            onChange={(e) => setTask(e.target.value)}
+                        />
                     </Form.Group>
+
                     <div className="border-top border-2 border-secondary my-2 p-2 d-flex flex-row justify-content-end">
                         <Button variant="secondary" onClick={onClose}>
                             Cance
