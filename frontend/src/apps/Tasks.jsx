fe0a30d1a65f87feb059a11ae5723ce6f0bf5fa4
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-26 12:19:01 +0800

    Create Task component init

--- frontend/src/apps/Tasks.jsx
+++ frontend/src/apps/Tasks.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import CreateTask from "../forms/CreateTask";
+
 function Tasks() {
+    const [create, setCreate] = useState(false);
 
     return (
         <>
-            <div>
-                Tasks App
+            <div className="d-flex flex-column">
+                <div className="p-1 d-flex flex-row justify-content-around">
+                    <div>Task App</div>
+                    <button
+                        onClick={() => setCreate(true)}
+                        className="btn btn-primary"
+                    >
+                        + Add Task
+                    </button>
+                </div>
+                <div>
+                    Tasks
+                </div>
             </div>
+            {create && <CreateTask />}
         </>
     )
 }
---
+++ frontend/src/forms/CreateTask.jsx
@@ -0,0 +1,11 @@
+function CreateTask() {
+    return (
+        <>
+            <div>
+                Create Task component
+            </div>
+        </>
+    )
+}
+
+export default CreateTask;
