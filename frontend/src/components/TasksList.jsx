afa32d0b5ab55d3d4ecfe2bca5f35577c66083f3
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-26 15:49:58 +0800

    tasks display status distinction

--- frontend/src/components/TasksList.jsx
+++ frontend/src/components/TasksList.jsx
@@ -2,10 +2,11 @@ function TasksList({tasks}) {
 
     return (
         <>
-            <div>
+            <div className="d-flex flex-column p-2">
                 {tasks.map((task, i) => (
                     <div
                         key={i}
+                        className={`${task.isDone === true ? "finished_tasks" : "unfinished_tasks"} tasks_list_div`}
                     >
                         {task.task}
                     </div>
