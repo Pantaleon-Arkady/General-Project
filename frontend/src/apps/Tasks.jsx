2000718411cbe34de3666550a8d3b1ed93defa9d
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-26 15:15:03 +0800

    Request for tasks *note get method uses url and auto transforms data into string

--- frontend/src/apps/Tasks.jsx
+++ frontend/src/apps/Tasks.jsx
@@ -1,8 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CreateTask from "../forms/CreateTask";
+import axios from "../api/axios";
+import { useAuth } from "../context/AuthContext";
 
 function Tasks() {
     const [create, setCreate] = useState(false);
+    const { user } = useAuth();
+    const [tasks, setTasks] = useState([]);
+
+    async function fetchTasks() {
+
+        try {
+            const res = await axios.get('/retrieve-tasks', {params: { user_id: user?.id }})
+
+            if (res.data.stat) {
+                setTasks(res.data.tasks);
+                console.log("Retrieved tasks successfully");
+                console.log(res.data.tasks);
+            }
+        } catch (err) {
+            console.log(err.response);
+        }
+    }
+
+    useEffect(() => {
+        if (user) {
+            fetchTasks();
+        }
+
+    }, [user])
 
     return (
         <>
@@ -16,8 +42,8 @@ function Tasks() {
                         + Add Task
                     </button>
                 </div>
-                <div>
-                    Tasks
+                <div className="notes_list_main_div p-2 d-flex flex-column bg-light">
+                    {tasks ? "Tasks retrieved" : "Task not retrieved"}
                 </div>
             </div>
             {
