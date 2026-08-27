9703202851f08e2fa9cacee10903a5c75f6df677
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-08-27 15:49:25 +0800

    Select form control by react bootstrap for dropdown form input

--- frontend/src/forms/EditTask.jsx
+++ frontend/src/forms/EditTask.jsx
@@ -5,8 +5,17 @@ function EditTask({ task, show, onClose }) {
     const [taskContent, setTaskContent] = useState(task.task);
     const [taskStatus, setTaskStatus] = useState(task.isDone);
 
-    const handleSubmit = () => {
+    const handleStatusChange = (e) => {
+        setTaskStatus(e.target.value === "true");
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
         console.log("Updating");
+        console.log("Task: ");
+        console.log(taskContent);
+        console.log("Status: ");
+        console.log(taskStatus);
     }
 
     return (
@@ -26,6 +35,21 @@ function EditTask({ task, show, onClose }) {
                             onChange={(e) => setTaskContent(e.target.value)}
                         />
                     </Form.Group>
+
+                    <Form.Group controlId="formGridState" className="mb-3">
+                        <Form.Label>Change status</Form.Label>
+                        <Form.Select 
+                            value={String(taskStatus)} 
+                            onChange={handleStatusChange}
+                        >
+
+                            <option value="" disabled>Status</option>
+                            <option value="true" >Done</option>
+                            <option value="false">Not Done</option>
+
+                        </Form.Select>
+                    </Form.Group>
+
                     <div className="border-top border-2 border-secondary my-2 p-2 d-flex flex-row justify-content-around">
                         <Button
                             onClick={onClose}
