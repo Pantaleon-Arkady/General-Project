fe86bf6d71341a6bb45c8b943485fc638b8fde13
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-09-03 17:16:17 +0800

    class init

---
+++ frontend/src/class/AuthClass.jsx
@@ -0,0 +1,38 @@
+class UserManager {
+
+    constructor() {
+
+        this.user = '';
+
+
+        // check in local storage if user exists
+    }
+
+    register(email, password) {
+        // fjdkslfdl
+
+        // set user to local storage
+        this.user = ''; //
+    }
+
+    login(username, password) {
+        // fetch post login
+        this.user = '';
+    }
+
+    logout() {
+        this.user = null;
+        // clear local storage
+    }
+
+}
+
+
+// const currentUserManager = new UserManager();
+
+
+
+// currentUserManager.register(email, password);
+
+
+// currentUserManager.login();
