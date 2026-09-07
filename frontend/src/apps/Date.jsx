1f01f9822dc47ad8c4d40ee2f057d76476b937b8
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-09-07 11:03:37 +0800

    Day date data

--- frontend/src/apps/Date.jsx
+++ frontend/src/apps/Date.jsx
@@ -2,12 +2,26 @@ function DateComponent() {
 
     const date = new Date(); 
 
+    const dateMonth = date.getMonth();
+
+    const dateMonthName = date.toLocaleString('default', { month: 'long' });
+
+    const dateDay = date.toLocaleDateString('en-US', { weekday: 'long' });
+
+    const dateOnly = date.toLocaleDateString();
+
+    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const dateDayIndex = daysOfTheWeek.indexOf(dateDay);
+
     return (
         <>
             <div>
                 Date App Component
                 <div>
-                    basic new date: {String(date)}
+                    basic new current date: {String(date)} - date only: {dateOnly}<br />
+                    month index: {dateMonth} - month name: {dateMonthName}<br />
+                    date day name: {dateDay} - index: {dateDayIndex}
                 </div>
             </div>
         </>
